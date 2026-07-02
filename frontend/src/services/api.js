478449738c9ca2a15d4182cf0323e3eea681f09a
Rohitha Ummadoju <rohitha.7u@gmail.com>
2026-07-02 12:22:36 +0530

    Update frontend API for Render deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://blood-donation-platform-5mjb.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
